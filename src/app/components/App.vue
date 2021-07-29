990c443bfffdb9b838b931fc47d67fc5ccbec756
David Olivero <dolivero@lebox.com>
2021-07-28 22:30:30 -0400

    Se agregan paquetes de babel webpack-cli y vue, se comienza a transpilar con vue configurando el webpack

---
+++ src/app/components/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand"> Users stack</a>
+        </nav>
+
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <!-- form-control estilizar inputs -->
+                                    <input type="text" placeholder="Insert names" class="form-control">
+                                </div>
+                                <!-- <div class="form-group">
+                                    <input type="text" placeholder="Insert first surname" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" placeholder="Insert second surname" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" placeholder="Insert username" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" placeholder="Insert RUN" class="form-control">
+                                </div>
+                                <div>
+                                    <input type="text" placeholder="Inser email" class="form-control">
+                                </div>
+                                <div>
+                                    <input type="password" placeholder="Insert password" class="form-control">
+                                </div> -->
+                                <button class="btn btn-primary btn-block">Save</button>
+
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
